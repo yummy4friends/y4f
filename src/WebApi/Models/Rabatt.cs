b008d87ec868f692ebef84b89bd6de51edf67d13
MET18937 <79221078+MET18937@users.noreply.github.com>
2023-05-21 00:50:08 +0200

    WebAPI for database integrity added
    Restructure the solution
    * The project has been split into the database integration "src/webapi" and the Blazor wasm project "src/y4f".
    
    WebAPI
    * API with CRUD capabilities
    
    * The main task of the "WebAPI" project is to access the database data and make it available to the Blazor-WASM project via an API.
    
    * GET example of the allergy table via the file "TestFetchAllergienData.razor", which can be tested via the browser "/TestFetchAllergienData".

---
+++ src/WebApi/Models/Rabatt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApi.Models;
+
+public partial class Rabatt
+{
+    public int Idrabatt { get; set; }
+
+    public decimal? Prozent { get; set; }
+
+    public DateTime? GueltigkeitVon { get; set; }
+
+    public DateTime? GueltigkeitBis { get; set; }
+
+    public virtual ICollection<Bestellungsposition> Bestellungspositions { get; set; } = new List<Bestellungsposition>();
+}
