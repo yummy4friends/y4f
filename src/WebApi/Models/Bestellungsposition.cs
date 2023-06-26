28327710f8a92e912958ebe521f3f9f60202f6f0
MET18937 <79221078+MET18937@users.noreply.github.com>
2023-06-26 18:55:12 +0200

    Warenkorb bestellungsabschluss + Login (Kunde/Chefin) (#48)
    * Add Bestellabschluss link
    
    * Add vorbestllungsdata into Table Bestellungsposition
    
    * Add vorbestllungsdata into Table BestellungspositionHasMenuitem
    
    * Add complete vorbestellungsfunktion
    
    * Add kontaktverwaltung Bestellübersicht
    
    * Update Bestelluebersicht BestelluebersichtD-Chefin
    
    * Update Bestelluebersicht
    
    * Fix kunde.Idkunde access
    
    * Add page acees only on valid kunde login
    
    * Add current kunde usage
    
    * Add admin login
    
    * Fix frontend speisekarte1
    
    * Fix treuepunkte vergabe
    
    * Fix chefin bestellungseinsicht

--- src/WebApi/Models/Bestellungsposition.cs
+++ src/WebApi/Models/Bestellungsposition.cs
@@ -1,23 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace WebApi.Models; 
+namespace WebApi.Models;
 
 public partial class Bestellungsposition
 {
-    public int Idbestellung { get; set; }
+	public int Idbestellung { get; set; }
 
-    public int? Menge { get; set; }
+	public int? Menge { get; set; }
 
-    public DateTime? Datum { get; set; }
+	public DateTime? Datum { get; set; }
 
-    public int KundeIdkunde { get; set; }
+	public int KundeIdkunde { get; set; }
 
-    public int RabattIdrabatt { get; set; }
+	public int? RabattIdrabatt { get; set; }
 
-    public virtual Kunde KundeIdkundeNavigation { get; set; } = null!;
+	public virtual Kunde? KundeIdkundeNavigation { get; set; } = null!;
 
-    public virtual Rabatt RabattIdrabattNavigation { get; set; } = null!;
+	public virtual Rabatt? RabattIdrabattNavigation { get; set; } = null!;
 
-    public virtual ICollection<Menuitem> MenuItemIdmenuItems { get; set; } = new List<Menuitem>();
+	public virtual ICollection<Menuitem>? MenuItemIdmenuItems { get; set; } = new List<Menuitem>();
 }
---
+++ src/WebApi/Models/BestellungspositionHasMenuitem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApi.Models;
+
+public partial class BestellungspositionHasMenuitem
+{
+	public int? Bestellungsposition_IDBestellung { get; set; }
+
+	public int? MenuItem_IDMenuItem { get; set; }
+}
